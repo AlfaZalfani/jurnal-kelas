8dc37ada6b8594a0fb2446a3c5e09f17b0539cbe
AlfaZalfani <alvan6383666@gmail.com>
2023-05-22 22:33:38 +0700

    add migration

---
+++ database/migrations/2023_05_21_142705_create_subjects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->nullable()->index('fk_subjects_to_teachers');
+            //still need to check
+            $table->string('subject_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
